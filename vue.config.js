f839a2adfc9ac39ed2aa3d0692c5d8db8ea9dba2
郭郭 <guoguo@guoguodeMacBook-Air.local>
2020-04-23 10:35:24 +0800

    新增首页底部栏

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    configureWebpack:{
+        resolve:{
+            alias:{//别名
+                'assets' : '@/assets',
+                'common' : '@/common',
+                'components' : '@/components',
+                'network' : '@/network',
+                'views' : '@/views',
+            }
+        }
+    }
+}
